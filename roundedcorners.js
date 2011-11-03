31d0e45fb35bf6c788b799c81d02908e421ae5be
Mark Van den Borre <mark@markvdb.be>
2011-11-03 16:34:31 +0100

    rounded corners test

--- roundedcorners.js
+++ roundedcorners.js
@@ -1,15 +1,18 @@
 $(function(){
 /*        $('dl.portletCalendar').wrap('<div class="outerPortletCalendar"></div>');
+*/
         $('dl.portletCalendar').each(function() {
             eval($(this).corner("round 8px").parent().css('padding', '8px').corner("round 14px"));
         });
-*/
         $('dl.portletStaticText').wrap('<div class="outerPortletStaticText"></div>');
+/*
         $('dl.portletStaticText').each(function() {
             eval($(this).corner("round 8px").parent().css('padding', '8px').corner("round 14px"));
         });
+*/
         $('div#content').wrap('<div class="outerContent"></div>');
-        $('div#content').each(function() {
+/*        $('div#content').each(function() {
             eval($(this).corner("round 8px").parent().css('padding', '8px').corner("round 14px"));
         });
+*/
 });
